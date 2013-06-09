44941ce3213bc3f5dee387f9a7897b6af54cd0bd
Lorenzo Rogai <lorenzo.rogai@hotmail.it>
2013-06-09 17:51:35 +0200

    Small typing mistake fix

--- Arduino PC Monitor/Form1.Designer.cs
+++ Arduino PC Monitor/Form1.Designer.cs
@@ -88,9 +88,9 @@
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(201, 8);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(87, 14);
+            this.label2.Size = new System.Drawing.Size(85, 14);
             this.label2.TabIndex = 2;
-            this.label2.Text = "Packets send";
+            this.label2.Text = "Packets sent";
             // 
             // label3
             // 
